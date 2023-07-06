67095cba64f31e9ee0bd949214ceeb9c27d56ac8
El-Savoury <ross_morgan@outlook.com>
2023-07-06 21:27:31 +0100

    Fixed merge markers

--- CollaborationTest/Game1.cs
+++ CollaborationTest/Game1.cs
@@ -4,81 +4,79 @@ using Microsoft.Xna.Framework.Input;
 
 namespace CollaborationTest
 {
-	public class Game1 : Game
-	{
-		const float SPEED = 300.0f;
-		private GraphicsDeviceManager _graphics;
-		private SpriteBatch _spriteBatch;
+    public class Game1 : Game
+    {
+        const float SPEED = 300.0f;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
 
-		Texture2D mIcon;
-		Vector2 mVel;
-		Vector2 mPos;
+        Texture2D mIcon;
+        Vector2 mVel;
+        Vector2 mPos;
 
-		public Game1()
-		{
-			_graphics = new GraphicsDeviceManager(this);
-			Content.RootDirectory = "Content";
-			IsMouseVisible = true;
-		}
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
 
-		protected override void Initialize()
-		{
-			mVel = new Vector2(SPEED, SPEED);
+        protected override void Initialize()
+        {
+            mVel = new Vector2(SPEED, SPEED);
 
-			base.Initialize();
-		}
+            base.Initialize();
+        }
 
-		protected override void LoadContent()
-		{
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			mIcon = Content.Load<Texture2D>("icon");
-		}
+            mIcon = Content.Load<Texture2D>("icon");
+        }
 
-		protected override void Update(GameTime gameTime)
-		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-				Exit();
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
-			Rectangle view = GraphicsDevice.Viewport.Bounds;
+            Rectangle view = GraphicsDevice.Viewport.Bounds;
 
-			if (mIcon.Height + mPos.Y >= view.Height)
-			{
-				mVel.Y = -SPEED;
-			}
-			else if(mPos.Y < 0.0f)
-			{
-				mVel.Y = SPEED;
-			}
+            if (mIcon.Height + mPos.Y >= view.Height)
+            {
+                mVel.Y = -SPEED;
+            }
+            else if (mPos.Y < 0.0f)
+            {
+                mVel.Y = SPEED;
+            }
 
-			if (mIcon.Width + mPos.X >= view.Width)
-			{
-				mVel.X = -SPEED;
-			}
-			else if (mPos.X < 0.0f)
-			{
-				mVel.X = SPEED;
-			}
+            if (mIcon.Width + mPos.X >= view.Width)
+            {
+                mVel.X = -SPEED;
+            }
+            else if (mPos.X < 0.0f)
+            {
+                mVel.X = SPEED;
+            }
 
-			mPos += mVel * gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
+            mPos += mVel * gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
 
-			base.Update(gameTime);
-		}
+            base.Update(gameTime);
+        }
 
-		protected override void Draw(GameTime gameTime)
-		{
-<<<<<<< HEAD
-			GraphicsDevice.Clear(Color.Yellow);
-=======
-			// This makes the game green! (DON@T DELETE THIS COMMENT ROSSS!!)
-			GraphicsDevice.Clear(Color.Yellow);
->>>>>>> 37ff5c52f965ba24d5d5b28e9e80cdaf9044050e
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Yellow);
 
-			_spriteBatch.Begin();
-			_spriteBatch.Draw(mIcon, mPos, Color.White);
-			_spriteBatch.End();
+            // Mah point!
+            GraphicsDevice.Clear(Color.Yellow);
 
-			base.Draw(gameTime);
-		}
-	}
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(mIcon, mPos, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
 }
