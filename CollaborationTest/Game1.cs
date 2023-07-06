769f11d8994803e3589eb9af8920ed33c425d546
adahlkvist <AugustDahlkvist@gmail.com>
2023-07-06 20:23:04 +0100

    First commit

---
+++ CollaborationTest/Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CollaborationTest
+{
+	public class Game1 : Game
+	{
+		const float SPEED = 100.0f;
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		Texture2D mIcon;
+		Vector2 mVel;
+		Vector2 mPos;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			mVel = new Vector2(SPEED, SPEED);
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			mIcon = Content.Load<Texture2D>("icon");
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			Rectangle view = GraphicsDevice.Viewport.Bounds;
+
+			if (mIcon.Height + mPos.Y >= view.Height)
+			{
+				mVel.Y = -SPEED;
+			}
+			else if(mPos.Y < 0.0f)
+			{
+				mVel.Y = SPEED;
+			}
+
+			if (mIcon.Width + mPos.X >= view.Width)
+			{
+				mVel.X = -SPEED;
+			}
+			else if (mPos.X < 0.0f)
+			{
+				mVel.X = SPEED;
+			}
+
+			mPos += mVel * gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.DarkGreen);
+
+			_spriteBatch.Begin();
+			_spriteBatch.Draw(mIcon, mPos, Color.White);
+			_spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ CollaborationTest/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new CollaborationTest.Game1();
+game.Run();
